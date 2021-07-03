cb3aec0ff088afe84ceba57c542915384f4326af
wimpyKid781 <71785664+wimpyKid781@users.noreply.github.com>
2021-07-02 17:04:16 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import Camera from './screens/camera';
+export default class App extends React.Component {
+  render() {
+
+  return (
+    <Camera />
+  );
+  }
+}
